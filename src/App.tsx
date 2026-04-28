fcd818e584219049fff0ed95933c7d5fba9e7f0b
Superhands <grant@superhands.ai>
2026-04-28 12:50:14 +0000

    Fix border radius on airport destination input field

--- src/App.tsx
+++ src/App.tsx
@@ -470,7 +470,7 @@ export function AirportField({
   // We use md: utilities to override mobile.
   const desktopSeam = isFirst
     ? 'md:rounded-r-none md:pr-[28px]'
-    : 'md:rounded-l-none md:pl-[38px] md:border-l-0'
+    : 'md:rounded-l-none md:rounded-r-[16px] md:pl-[38px] md:border-l-0'
   // When open, on desktop, the first airport needs purple right border; the second needs purple inset-left.
   const openSeamClasses = open
     ? isFirst
